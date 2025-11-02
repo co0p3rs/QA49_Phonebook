7b632799486b66f4ae99d5e2b51166b037b1ccbc
Dmytro <dmytro.podoprygora95@gmail.com>
2025-11-02 09:43:31 +0200

    Your commit message here

--- src/test/java/ui_tests/AddNewContactTests.java
+++ src/test/java/ui_tests/AddNewContactTests.java
@@ -1,12 +1,16 @@
 package ui_tests;
 
+import dto.Contact;
 import manager.ApplicationManager;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.*;
+import utils.ContactFactory;
 import utils.HeaderMenuItem;
 
 import static pages.BasePage.*;
+import static utils.PropertiesReader.*;
 
 public class AddNewContactTests extends ApplicationManager {
 
@@ -14,18 +18,34 @@ public class AddNewContactTests extends ApplicationManager {
     LoginPage loginPage;
     ContactsPage contactsPage;
     AddPage addPage;
+    int numberOfContacts;
 
     @BeforeMethod
     public void login(){
         homePage = new HomePage(getDriver());
         loginPage = clickButtonHeader(HeaderMenuItem.LOGIN);
-        loginPage.typeLoginForm("a@mail.ru", "Password123!");
+        //loginPage.typeLoginForm("a@mail.ru", "Password123!");
+        loginPage.typeLoginForm(getProperty("properties/base.properties", "login"),
+                getProperty("properties/base.properties", "password"));
         contactsPage = new ContactsPage(getDriver());
+        numberOfContacts = contactsPage.getNumberOfContacts();
         addPage = clickButtonHeader(HeaderMenuItem.ADD);
     }
 
     @Test
     public void addNewContactPositiveTest(){
-
+        addPage.typeContactForm(ContactFactory.positiveContact());
+        int numberOfContactsAfterAdd = contactsPage.getNumberOfContacts();
+        Assert.assertEquals(numberOfContactsAfterAdd, numberOfContacts + 1);
     }
+
+    @Test
+    public void addNewContactPositiveTestValidateList(){
+        Contact contact = ContactFactory.positiveContact();
+        addPage.typeContactForm(contact);
+        //contactsPage.clickLastContact();
+        Assert.assertTrue(contactsPage.isContactPresent(contact));
+    }
+
+
 }
